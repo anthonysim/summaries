85906a9d1499bfe664d4bd17af3d41040c99007b
Anthony Sim <anthonyysim@gmail.com>
2025-08-05 01:59:29 -0700

    update Input import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "./components/Input/Input";
+import Input from "./components/Input";
 
 export default function App() {
   const [userInput, setUserInput] = useState<string>("");
